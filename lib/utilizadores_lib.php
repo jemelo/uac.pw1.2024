13350952ed09a45b5085c31d91dd22dc28aa9530
José Melo (macbook pro) <jemelo@controlaltdelete.pt>
2024-11-14 09:31:08 -0100

    adicionar cookies para inicio de sessão

--- lib/utilizadores_lib.php
+++ lib/utilizadores_lib.php
@@ -14,8 +14,12 @@ function validaUtilizador(string $username, string $password): array|bool
 
         if ($username == $utilizador[0]) {
             if (password_verify($password, $utilizador[1])) {
-                session_start();
+                @session_start();
                 $_SESSION['nome'] = $utilizador[2];
+                setcookie('tarefaslogin', json_encode([
+                    'utilizador' => $username,
+                    'password' => $password,
+                ]), time()+ 60);
                 return $utilizador;
             } else {
                 return false;
@@ -28,9 +32,15 @@ function validaUtilizador(string $username, string $password): array|bool
 
 function validaSessao(): bool
 {
-    session_start();
+    @session_start();
     if (empty($_SESSION) || empty($_SESSION['nome'])) {
-        return false;
+        if (isset($_COOKIE['tarefaslogin'])) {
+            $dadosCookie = json_decode($_COOKIE['tarefaslogin'], true);
+            $utilizador = validaUtilizador($dadosCookie['utilizador'], $dadosCookie['password']);
+            return is_array($utilizador) ? true : $utilizador;
+        } else {
+            return false;
+        }
     }
 
     return true;
@@ -41,6 +51,8 @@ function terminaSessao(): bool
     if (!validaSessao()) {
         return true;
     }
+
+    setcookie('tarefaslogin', '', time()-1);
 
     $_SESSION = [];
     session_destroy();
